950a47f96140b883c7843af0c281cb5f583ac117
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2024-07-18 23:29:41 +0800

    chore: Refactor home screen and add bottom navigation bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,34 +1,70 @@
+import 'package:assessment_2_app/screens/favorite.dart';
+import 'package:assessment_2_app/screens/posts_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class HomeScreen extends StatelessWidget {
-  final Function(int) onTabSelected; // Callback to switch tabs
+import '../widgets/bottom_navbar.dart';
+import 'user_profile.dart';
 
-  const HomeScreen({super.key, required this.onTabSelected});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeContent(),
+    const PostsList(),
+    const FavoriteScreen(),
+    const UserProfileScreen(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTabTapped: _onTabTapped,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Welcome to Your App!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: const Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              'Welcome to Post App!',
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                onTabSelected(1); // Switch to Posts tab
-              },
-              child: const Text('View Posts'),
+            SizedBox(height: 20),
+            Text(
+              'This is a simple app that displays a list of posts. You can view the details of each post, mark them as favorites, and search for posts.',
+              style: TextStyle(fontSize: 16, color: Colors.black54),
             ),
-          ],
-        ),
-      ),
+            SizedBox(height: 30),
+          ])),
     );
   }
 }
