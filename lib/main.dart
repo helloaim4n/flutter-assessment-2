d3bbc3c8c0b0eb2e29c5587fe07b77b4d5ac6468
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2024-07-18 23:29:34 +0800

    chore: Remove unused screens and refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:assessment_2_app/providers/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/home.dart';
-import 'screens/posts_list.dart';
-import 'screens/user_profile.dart';
-import 'screens/favorite.dart';
 import 'utils/theme.dart';
 
 void main() {
@@ -21,66 +18,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter App',
         theme: AppTheme.lightTheme, // Apply the theme here
-        home: const MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-
-  final List<Widget> _screens = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _screens.addAll([
-      HomeScreen(onTabSelected: _onTabTapped),
-      const PostsList(),
-      const FavoriteScreen(),
-      const UserProfileScreen(),
-    ]);
-  }
-
-  void _onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTabTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.article),
-            label: 'Posts',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+        home: const HomeScreen(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
