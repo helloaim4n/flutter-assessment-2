1782631d6d97a325db8fd9ce53272e7e228caecf
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2024-07-19 10:18:20 +0800

    chore: Refactor main.dart and add navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:assessment_2_app/providers/app_state.dart';
+import 'package:assessment_2_app/screens/posts_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'screens/favorite.dart';
 import 'screens/home.dart';
+import 'screens/user_profile.dart';
 import 'utils/theme.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AppState(),
-      child: MaterialApp(
-        title: 'Flutter App',
-        theme: AppTheme.lightTheme, // Apply the theme here
-        home: const HomeScreen(),
-        debugShowCheckedModeBanner: false,
-      ),
-    );
-  }
+  runApp(ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: Consumer<AppState>(builder: (context, appState, _) {
+        return MaterialApp(
+            title: 'Flutter App',
+            theme: AppTheme.lightTheme, // Apply the theme here
+            home: const HomeScreen(),
+            debugShowCheckedModeBanner: false,
+            routes: {
+              '/posts': (context) => const PostsList(),
+              '/favorites': (context) => const FavoriteScreen(),
+              '/profile': (context) => const UserProfileScreen(),
+            });
+      })));
 }
