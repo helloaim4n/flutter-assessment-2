677ca1afec7a7f8727fe726767047a6f72175aa6
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2024-07-19 12:25:56 +0800

    chore: Add profile header and user info section to UserProfileScreen

--- lib/screens/user_profile.dart
+++ lib/screens/user_profile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import '../models/user.dart'; // Import your User model here
-import '../services/api_service.dart'; // Import your ApiService here
+import '../models/user.dart';
+import '../services/api_service.dart';
+import '../widgets/profile_header.dart';
+import '../widgets/user_info_section.dart';
 
 class UserProfileScreen extends StatelessWidget {
   const UserProfileScreen({super.key});
@@ -12,29 +14,26 @@ class UserProfileScreen extends StatelessWidget {
         title: const Text('User Profile'),
       ),
       body: FutureBuilder<User>(
-        future: fetchUser(1), // Replace 1 with the actual userId
+        future: fetchUser(1),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.hasError) {
               return const Center(
-                  child: Text("Error fetching user information"));
+                child: Text("Error fetching user information"),
+              );
             }
             final user = snapshot.data!;
-            return Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Name: ${user.name}',
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 10),
-                  Text('Username: ${user.username}'),
-                  const SizedBox(height: 10),
-                  Text('Email: ${user.email}'),
-                ],
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ProfileHeader(user: user),
+                    const SizedBox(height: 20),
+                    UserInfoSection(user: user),
+                  ],
+                ),
               ),
             );
           } else {
