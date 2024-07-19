dd3468f917cbabf554901f8f0fab297e9b770e1f
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2024-07-19 10:19:57 +0800

    feat: Add favorite button to PostCard widget

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../models/post.dart';
+import '../providers/app_state.dart';
 import '../screens/post_detail.dart';
 
 class PostCard extends StatelessWidget {
   final Post post;
 
-  const PostCard({super.key, required this.post});
+  const PostCard({
+    super.key,
+    required this.post,
+  });
 
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+    bool isFavorite = appState.favorites.contains(post);
+
     return Card(
-      margin: const EdgeInsets.all(10),
-      elevation: 5,
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: ListTile(
         title: Text(post.title),
         subtitle: Text(post.body),
+        trailing: IconButton(
+          icon: Icon(
+            isFavorite ? Icons.favorite : Icons.favorite_border,
+            color: isFavorite ? Colors.red : null,
+          ),
+          onPressed: () {
+            appState.toggleFavorite(post);
+          },
+        ),
         onTap: () {
           Navigator.push(
             context,
